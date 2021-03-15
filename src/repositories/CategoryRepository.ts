4a4be99e97398bfa05eea08a89d0e2a880329c1e
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-15 17:46:54 -0300

    feat: Create Category Repository

---
+++ src/repositories/CategoryRepository.ts
@@ -0,0 +1,20 @@
+import { ICategoryDTO } from '../dtos/ICategoryDTO';
+import { Category } from '../models/Category';
+
+class CategoryRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  public create({ name, description }: ICategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, { name, description, created_at: new Date() });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoryRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,16 @@
 import { Router } from 'express';
 
-import { Category } from '../models/Category';
+import { CategoryRepository } from '../repositories/CategoryRepository';
 
 const categoriesRouter = Router();
-
-const categories: Category[] = [];
+const categoryRepository = new CategoryRepository();
 
 categoriesRouter.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoryRepository.create({ name, description });
 
-  Object.assign(category, { name, description, created_at: new Date() });
-
-  categories.push(category);
-
-  return response.status(201).json(category);
+  return response.status(201).send();
 });
 
 export { categoriesRouter };

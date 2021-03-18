a6d7184d80f9bac273e6948eb64e465da73c4e31
rennand <rplaykl@gmail.com>
2021-03-18 12:22:15 -0300

    feat: crate csv file stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,21 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
-  public execute(file: any): void {
-    console.log(file);
+  public execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
 

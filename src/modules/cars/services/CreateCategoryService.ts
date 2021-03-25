f97cbc6a11b4fa8a86bbc6b3d876d12b80a0001a
RennanD <rennan.douglas@objetivopi.com.br>
2021-03-25 13:13:28 -0300

    feat: add environment with docker

---
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoryRepository: ICategoriesRepository) {}
+
+  public execute({ name, description }: IRequest): void {
+    const existentCategory = this.categoryRepository.findByName(name);
+
+    if (existentCategory) {
+      throw new Error('Category already exists');
+    }
+
+    this.categoryRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };

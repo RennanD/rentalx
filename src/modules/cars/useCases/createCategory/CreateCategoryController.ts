b6c8492f3a0bc7246c035af3e17223e5d5a7e1b4
rennand <rplaykl@gmail.com>
2021-04-02 18:07:03 -0300

    feat: configure tsyring for dependency injection

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCategoryController {
-  constructor(private createCategory: CreateCategoryUseCase) {}
-
   public async handle(request: Request, response: Response): Promise<Response> {
     try {
       const { name, description } = request.body;
-
-      await this.createCategory.execute({
+      const createCategory = container.resolve(CreateCategoryUseCase);
+      await createCategory.execute({
         description,
         name,
       });

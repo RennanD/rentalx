9ad013f43b2594bef4fec99fbcd05dc09ae972f1
RennanD <rennan.douglas@objetivopi.com.br>
2021-04-13 14:45:44 -0300

    feat: divide errors by responsibilities

---
+++ src/shared/errors/ServerError.ts
@@ -0,0 +1,13 @@
+class UnauthorizedError {
+  public readonly message: string;
+  public readonly status_code: number;
+  public readonly type_error: string;
+
+  constructor(message: string) {
+    this.message = message;
+    this.status_code = 500;
+    this.type_error = 'server_error';
+  }
+}
+
+export { UnauthorizedError };
---
+++ src/shared/errors/UnauthorizedError.ts
@@ -0,0 +1,13 @@
+class UnauthorizedError {
+  public readonly message: string;
+  public readonly status_code: number;
+  public readonly type_error: 'auth_error' | 'expired_token';
+
+  constructor(message: string, type_error: 'auth_error' | 'expired_token') {
+    this.message = message;
+    this.status_code = 401;
+    this.type_error = type_error;
+  }
+}
+
+export { UnauthorizedError };
